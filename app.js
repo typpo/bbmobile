8b18e143892556dea48b2c0116cb1e6d3670ce2d
typpo <ian@typpo.us>
2012-05-07 18:06:34 -0400

    frontend

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require('express')
   , app = express.createServer()
   , OAuth = require('oauth').OAuth
 
-
 // Express config
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
@@ -14,32 +13,40 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.bodyParser());
 app.use(express.session({secret: "asdklasl3"}));
 
+// Oauth config
+var oa = new OAuth("https://www.boredatbaker.com/api/v1/oauth/request_token",
+            "https://www.boredatbaker.com/api/v1/oauth/access_token",
+            "31324ebdc01f728d820a282ab04d555604fa2ff3f",
+            "a0997237fe6e9363407df63eaa05f190",
+            "1.0",
+            "http://ianww.com:10000/oauth_cb",
+            "HMAC-SHA1");
+
 // App
 
 function require_login(req, res, next) {
   if(!req.session.oauth_access_token) {
-    res.redirect("/l"); //?action="+querystring.escape(req.originalUrl));
+    req.session.destroy();
+    res.redirect("/login"); //?action="+querystring.escape(req.originalUrl));
     return;
   }
   next();
 };
 
 
-app.get('/', function(req, res) {
-  res.render('index', {
-
-  });
+app.get('/', require_login, function(req, res) {
+  oa.get("http://www.boredatbaker.com/api/v1/posts",
+    req.session.oauth_access_token,
+    req.session.oauth_access_token_secret,
+    function (error, data, response) {
+      var feed = JSON.parse(data);
+      res.render('index', {
+        data: feed,
+      });
+    });
 });
 
 app.get('/oauth_cb', function(req, res) {
-  var oa = new OAuth(req.session.oa._requestUrl,
-                    req.session.oa._accessUrl,
-                    req.session.oa._consumerKey,
-                    req.session.oa._consumerSecret,
-                    req.session.oa._version,
-                    req.session.oa._authorize_callback,
-                    req.session.oa._signatureMethod);
-
   oa.getOAuthAccessToken(
     req.session.oauth_token,
     req.session.oauth_token_secret,
@@ -48,33 +55,26 @@ app.get('/oauth_cb', function(req, res) {
 
       if(error) {
         console.error(error);
+        req.session.destroy();
+        res.send('There was an error trying to request a token from b@b');
       }
       else {
         // store the access token in the session
         req.session.oauth_access_token = oauth_access_token;
         req.session.oauth_access_token_secret = oauth_access_token_secret;
-
-        res.send('good');
+        res.redirect('/');
       }
     });
 });
 
 
-app.get('/l', function(req, res) {
-  var getRequestTokenUrl = "https://www.boredatbaker.com/api/v1/oauth/request_token";
-
-
-  var oa = new OAuth(getRequestTokenUrl,
-                    "https://www.boredatbaker.com/api/v1/oauth/access_token",
-                    "31324ebdc01f728d820a282ab04d555604fa2ff3f",
-                    "a0997237fe6e9363407df63eaa05f190",
-                    "1.0",
-                    "http://ianww.com:10000/oauth_cb",
-                    "HMAC-SHA1");
+app.get('/login', function(req, res) {
 
   oa.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results){
     if(error) {
       console.error(error);
+      req.session.destroy();
+      res.send('There was an error talking to the b@b server');
     }
     else {
       // store the tokens in the session
@@ -89,22 +89,6 @@ app.get('/l', function(req, res) {
 });
 
 app.get('/posts', require_login, function(req, res) {
-  var oa = new OAuth(req.session.oa._requestUrl,
-                    req.session.oa._accessUrl,
-                    req.session.oa._consumerKey,
-                    req.session.oa._consumerSecret,
-                    req.session.oa._version,
-                    req.session.oa._authorize_callback,
-                    req.session.oa._signatureMethod);
-
-  oa.get("http://www.boredatbaker.com/api/v1/posts",
-    req.session.oauth_access_token,
-    req.session.oauth_access_token_secret,
-    function (error, data, response) {
-
-      var feed = JSON.parse(data);
-      res.send(feed);
-    });
 });
 
 
