989ed0efc772922e01dae774ab03f0d3c34329e6
Ian <typppo@gmail.com>
2012-05-28 13:23:56 -0700

    update for new xcode and try to make links open in safari

--- client/ios/bbmobile/bbmobile/Classes/AppDelegate.m
+++ client/ios/bbmobile/bbmobile/Classes/AppDelegate.m
@@ -132,6 +132,22 @@
     return YES;    
 }
 
+- (BOOL)webView:(UIWebView *)theWebView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
+{
+	NSURL *url = [request URL];
+    
+	// Intercept the external http requests and forward to Safari.app
+	// Otherwise forward to the PhoneGap WebView
+    // Avoid opening iFrame URLs in Safari by inspecting the `navigationType`
+	if ((navigationType == UIWebViewNavigationTypeLinkClicked && [[url scheme] isEqualToString:@"http"]) || [[url scheme] isEqualToString:@"https"]) {
+		[[UIApplication sharedApplication] openURL:url];
+		return NO;
+	}
+	else {
+		return [ super webView:theWebView shouldStartLoadWithRequest:request navigationType:navigationType ];
+	}
+}
+
 - (void) dealloc
 {
 	[super dealloc];
